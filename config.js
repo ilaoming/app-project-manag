a6cfb02f99c17792c17d51da8d77a0468d224535
ilaoming <jesus.rios2609@gmail.com>
2022-01-30 11:25:54 -0500

    First Commit

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database:{
+        connectionLimit: 20,
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD ||  '',
+        database: process.env.DB_DATABASE || 'project_management'
+    }
+}
---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const pool = require('./mysql');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.get('/product', (req,res) =>{
+    pool.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+       
+        // Use the connection
+        connection.query('SELECT * FROM product', function (error, results, fields) {
+          // When done with the connection, release it.
+          connection.release();
+       
+          // Handle error after the release.
+          if (error) throw error;
+
+          //results
+          res.json(results);
+        });
+    });
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on PORT ${PORT}`);
+})
+
