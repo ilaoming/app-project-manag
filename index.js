b944a7240c21753a4a235a51dc1d1138b1589f82
ilaoming <jesus.rios2609@gmail.com>
2022-01-30 13:22:52 -0500

    Change API /Product to /Service_order

--- index.js
+++ index.js
@@ -4,16 +4,30 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const pool = require('./mysql');
 
+//Home
 app.get('/', (req, res) => {
   res.send('Hello World!, Heroku');
 })
 
-app.get('/product', (req,res) =>{
+
+//GET all service order
+app.get('/service-order', (req,res) =>{
     pool.getConnection(function(err, connection) {
         if (err) throw err; // not connected!
-       
+
+        //Query SQL
+        const query = `
+         SELECT * FROM
+         service_order
+         INNER JOIN
+         product
+         WHERE 
+         product.order_id = service_order.id
+        `
+
+
         // Use the connection
-        connection.query('SELECT * FROM product', function (error, results, fields) {
+        connection.query(query, function (error, results, fields) {
           // When done with the connection, release it.
           connection.release();
        
